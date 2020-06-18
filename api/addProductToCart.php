25740bce28c004274ab109ec413e73add8b7d27d
Student <student@Priyanka-Siriki.local>
2020-06-18 18:26:30 -0400

    added functionality for the user to change the quantity of the product in the cart

--- api/addProductToCart.php
+++ api/addProductToCart.php
@@ -1,22 +1,22 @@
 <?php
- ini_set("display_errors", 1);
- ini_set("track_errors", 1);
- ini_set("html_errors", 1);
- error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
 
 header('Content-type: application/json');
 $localhost = 'localhost';
-    $db_user = 'root';
-    $db_password = '';
-    $db_name = 'shopoo';
-  $GLOBALS['db'] = new PDO('mysql:host='.$localhost.';dbname='.$db_name.';',$db_user,$db_password); 
-  $GLOBALS['db']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db_user = 'root';
+$db_password = '';
+$db_name = 'shopoo';
+$GLOBALS['db'] = new PDO('mysql:host=' . $localhost . ';dbname=' . $db_name . ';', $db_user, $db_password);
+$GLOBALS['db']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  class CheckProductsQuantity{
+class CheckProductsQuantity
+{
 
     private $conn;
     private $table = 'Products';
-    private $cartTable = 'Cart';
 
     public $product_id;
     public $size;
@@ -27,12 +27,13 @@ $localhost = 'localhost';
     public $picture;
     public $available_quantity;
 
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->conn = $db;
-
     }
-    
-    public function readProductsData(){
+
+    public function readProductsData($prod_id)
+    {
         $query = 'SELECT 
                 product_id,
                 size,
@@ -43,20 +44,19 @@ $localhost = 'localhost';
                 picture,
                 available_quantity 
                 FROM 
-                ' .$this->table.' where product_id = ? LIMIT 1';
+                ' . $this->table . ' where product_id = ? LIMIT 1';
 
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(1, $this->product_id);
+        $stmt->bindParam(1, $prod_id);
         $stmt->execute();
 
         return $stmt;
-    
     }
-
 }
 
 
-class getAddressId{
+class getAddressId
+{
 
     private $conn;
     private $table = 'Address';
@@ -70,11 +70,14 @@ class getAddressId{
     public $city;
     public $phone;
 
-    public function __construct($db){
-        $this->conn = $db;}
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
 
-    public function readAddressId(){
+    public function readAddressId($usr_id)
+    {
         $query = 'SELECT 
                 address_id,
                 user_id,
@@ -85,65 +88,118 @@ class getAddressId{
                 city,
                 phone
                 FROM 
-                ' .$this->table.' where user_id = ? LIMIT 1';
+                ' . $this->table . ' where user_id = ? LIMIT 1';
 
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(1, $this->user_id);
+        $stmt->bindParam(1, $usr_id);
         $stmt->execute();
 
         return $stmt;
-    
     }
 }
 
-class createCartRec{
+class createCartRec
+{
 
     private $conn;
     private $table = 'Cart';
 
     public $cart_id;
+    public $product_id;
     public $user_id;
     public $address_id;
     public $quantity;
     public $time_added;
-   
 
-    public function __construct($db){
+
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
 
-    public function createCart($address_id, $user_id,$quantity){
-
-        $query = 'INSERT INTO ' . $this->table .
-                '( user_id, address_id, quantity, time_added)
-                VALUES ( :user_id, :address_id, :quantity, now())';
+    public function isProductExistsInCart($usr_id, $prod_id)
+    {
+        $query = 'SELECT 
+                cart_id,
+                product_id,
+                user_id,
+                address_id,
+                quantity,
+                time_added
+                FROM 
+                ' . $this->table . ' where user_id = ? and product_id = ? LIMIT 1';
 
         $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $usr_id);
+        $stmt->bindParam(2, $prod_id);
+        try {
+            if ($stmt->execute()) {
+                return $stmt;
+            }
+
+
+            printf("Error %s. \n", $stmt->error);
+            return null;
+        } catch (Exception $e) {
+            echo $e;
+        }
+    }
+
+    public function updateQuantity($usr_id, $prod_id, $prev_quantity_of_Product, $quantity)
+    {
+        $query = 'UPDATE ' . $this->table . '
+                SET quantity = :quantity
+                 where user_id = :user_id and product_id = :product_id LIMIT 1';
+
+        $stmt = $this->conn->prepare($query);
+        $final_quantity = $prev_quantity_of_Product + $quantity;
+        $stmt->bindParam(':quantity', $final_quantity);
+        $stmt->bindParam(':user_id', $usr_id);
+        $stmt->bindParam(':product_id', $prod_id);
+
+        try {
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            printf("Error %s. \n", $stmt->error);
+            return false;
+        } catch (Exception $e) {
+            echo 'issue in executing the update statement' . $e;
+        }
+    }
+
+    public function createCart($product_id, $address_id, $user_id, $quantity)
+    {
+
+        $query = 'INSERT INTO ' . $this->table .
+            '( product_id, user_id, address_id, quantity, time_added)
+                VALUES ( :product_id, :user_id, :address_id, :quantity, now())';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':product_id', $product_id);
         $stmt->bindParam(':user_id', $user_id);
-        $stmt->bindParam(':address_id', $address_id); 
+        $stmt->bindParam(':address_id', $address_id);
         $stmt->bindParam(':quantity', $quantity);
 
-        try{
-            if($stmt->execute()){
+        try {
+            if ($stmt->execute()) {
                 return true;
-        }
+            }
 
-        printf("Error %s. \n", $stmt->error);
-        return false;
+            printf("Error %s. \n", $stmt->error);
+            return false;
+        } catch (Exception $e) {
+            echo 'issue in executing the insert statement' . $e;
         }
-        catch(Exception $e){
-            echo 'issue in executing the statement'.$e;
-        }
-        
-    
     }
 }
 
 
-  function isProductAvailable($product_id)
-  {
+function isProductAvailable($product_id)
+{
     $isProdAvailable = false;
-   
+
     header('Access-COntrol-Allow-Origin: *');
     header('Content-type: application/json');
 
@@ -151,15 +207,15 @@ class createCartRec{
 
     $checkQuantity->product_id = $product_id;
 
-    $result = $checkQuantity->readProductsData();
+    $result = $checkQuantity->readProductsData($product_id);
 
     $num = $result->rowCount();
 
-    if($num > 0){
+    if ($num > 0) {
         $checkQuantity_arr = array();
         $checkQuantity_arr['data'] = array();
 
-        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
             $checkQuantity_item = array(
                 'product_id' => $product_id,
@@ -173,32 +229,28 @@ class createCartRec{
             );
 
             array_push($checkQuantity_arr['data'], $checkQuantity_item);
-
-
         }
 
         echo json_encode($checkQuantity_arr);
-
-
-    }else{
-        echo json_encode(array('message' => 'no available products'));   
+    } else {
+        echo json_encode(array('message' => 'no available products'));
     }
 
+    if ($available_quantity > 0) {
 
+        return $available_quantity;
+        
+    } else {
 
-    if($available_quantity > 0){
-        $isProdAvailable = true;
-    }
-    else{
+        return null;
         echo 'Product is not available. Please try later!';
-        $isProdAvailable =false;
+     
     }
+    
+}
 
-
-    return $isProdAvailable;
-  }
-
-  function fetchAddressOfUser($user_id){
+function fetchAddressOfUser($user_id)
+{
 
     header('Access-COntrol-Allow-Origin: *');
     header('Content-type: application/json');
@@ -207,15 +259,15 @@ class createCartRec{
 
     $getAddressId->user_id = $user_id;
 
-    $result = $getAddressId->readAddressId();
+    $result = $getAddressId->readAddressId($user_id);
 
     $num = $result->rowCount();
 
-    if($num > 0){
+    if ($num > 0) {
         $getAddressId_arr = array();
         $getAddressId_arr['data'] = array();
 
-        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
             $getAddressId_item = array(
                 'address_id' => $address_id,
@@ -229,30 +281,86 @@ class createCartRec{
             );
 
             array_push($getAddressId_arr['data'], $getAddressId_item);
-
-
         }
 
-        echo json_encode($getAddressId_arr);
+        //echo json_encode($getAddressId_arr);
 
 
-    }else{
-        echo json_encode(array('message' => 'no available products'));   
+    } else {
+        echo json_encode(array('message' => 'no address for user exists'));
     }
 
-    if($address_id != null){
+    if ($address_id != null) {
         return $address_id;
-        
-    }
-    else{
+    } else {
         echo 'Failure to fetch address id for the user!';
         return null;
     }
+}
 
-  }
+function checkIfProductAlreadyExistsInCart($usr_id, $prod_id)
+{
+    header('Access-COntrol-Allow-Origin: *');
+    header('Content-type: application/json');
 
-  function createCartEntry($address_id, $user_id,$quantity){
-   
+    $checkProdExists = new createCartRec($GLOBALS['db']);
+
+    $checkProdExists->user_id = $usr_id;
+    $checkProdExists->product_id = $prod_id;
+
+    $result = $checkProdExists->isProductExistsInCart($usr_id, $prod_id);
+
+    $num = $result->rowCount();
+
+    if ($num > 0) {
+        $getQuantity_arr = array();
+        $getQuantity_arr['data'] = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $getQuantity_item = array(
+                'cart_id' => $cart_id,
+                'product_id' => $product_id,
+                'user_id' => $user_id,
+                'address_id' => $address_id,
+                'quantity' => $quantity,
+                'time_added' => $time_added
+            );
+
+            array_push($getQuantity_arr['data'], $getQuantity_item);
+        }
+
+        //echo json_encode($getQuantity_arr);
+
+        return $quantity;
+    } else {
+        //echo json_encode(array('message' => 'product already doesnt exists in cart'));
+        return null;
+    }
+}
+
+function updateProductQuantity($user_id, $product_id, $prev_quantity_of_Product, $quantity)
+{
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+    $updateCartRecord = new createCartRec($GLOBALS['db']);
+
+
+    if ($updateCartRecord->updateQuantity($user_id, $product_id, $prev_quantity_of_Product, $quantity)) {
+        echo "Update successful!";
+    } else {
+        echo 'Update is unsuccessful!';
+    }
+}
+
+
+function createCartEntry($product_id, $address_id, $user_id, $quantity)
+{
+
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
@@ -260,43 +368,40 @@ class createCartRec{
 
 
     $createCartRecord = new createCartRec($GLOBALS['db']);
-    
 
-    if($createCartRecord->createCart($address_id,$user_id,$quantity)){
-        echo "</br>";
-        echo "\n Insert successful!";
-    }
-    else{
+
+    if ($createCartRecord->createCart($product_id, $address_id, $user_id, $quantity)) {
+        echo "Insert successful!";
+    } else {
         echo 'Insert is unsuccessful!';
     }
-  }
+}
 
-  $http_verb = strtolower($_SERVER['REQUEST_METHOD']);
+$http_verb = strtolower($_SERVER['REQUEST_METHOD']);
 
-  if ($http_verb == 'post')
-  {
-    try
-    {
+if ($http_verb == 'post') {
+    try {
         $post = trim(file_get_contents("php://input"));
         $json = json_decode($post, true);
-            
+
         $product_id = $json['product_id'];
         $user_id = $json['user_id'];
         $quantity = $json['quantity'];
-        
-        $output = isProductAvailable($product_id);
 
-        if($output){
+
+
+        if (isProductAvailable($product_id)) {
             $fetchedAddressId = fetchAddressOfUser($user_id);
-            if($fetchedAddressId != null){
-                $insert = createCartEntry($fetchedAddressId, $user_id, $quantity);
+            if ($fetchedAddressId != null) {
+                $prev_quantity_of_Product = checkIfProductAlreadyExistsInCart($user_id, $product_id);
+                if ($prev_quantity_of_Product != null) {
+                    $update = updateProductQuantity($user_id, $product_id, $prev_quantity_of_Product, $quantity);
+                } else {
+                    $insert = createCartEntry($product_id, $fetchedAddressId, $user_id, $quantity);
+                }
             }
         }
-        
-    }
-    catch (Exception $e) 
-    {
-      echo $e;
-      
+    } catch (Exception $e) {
+        echo $e;
     }
 }
